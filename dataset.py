344c6a01a9b43a4f81f3a4cd2cc72054d649de3c
Markus Enzweiler <markus.enzweiler@hs-esslingen.de>
2023-12-24 01:18:29 +0100

    clean-up in cifar loader

--- dataset.py
+++ dataset.py
@@ -10,14 +10,16 @@ import torchvision.transforms as transforms
 
 def cifar(batch_size, cifar_version="CIFAR-10", root="./data"):
     if (cifar_version == "CIFAR-10"):
-        return cifar10(batch_size, root)
+        load_fn = torchvision.datasets.CIFAR10
+        return cifar_loader(batch_size, load_fn, root)
     elif (cifar_version == "CIFAR-100"):
-        return cifar100(batch_size, root)
+        load_fn = torchvision.datasets.CIFAR100
+        return cifar_loader(batch_size, load_fn, root)
     else:
         raise ValueError(f"Unknown CIFAR version: {cifar_version}")
     
 
-def cifar10(batch_size, root):
+def cifar_loader(batch_size, load_fn, root):
 
     transform = transforms.Compose(
         [transforms.Resize((32, 32)),  # resize to 64x64 pixels
@@ -25,8 +27,8 @@ def cifar10(batch_size, root):
         ])
 
     # load train and test sets using torchvision
-    tr   = torchvision.datasets.CIFAR10(root=root, train=True,   download=True, transform=transform)
-    test = torchvision.datasets.CIFAR10(root=root, train=False,  download=True, transform=transform)
+    tr   = load_fn(root=root, train=True,   download=True, transform=transform)
+    test = load_fn(root=root, train=False,  download=True, transform=transform)
    
     # Data loaders
     train_loader = torch.utils.data.DataLoader(tr, batch_size=batch_size,
@@ -35,28 +37,7 @@ def cifar10(batch_size, root):
     test_loader  = torch.utils.data.DataLoader(test, batch_size=batch_size,
                                                shuffle=False, pin_memory=True, num_workers=2)
     
-    return train_loader, test_loader, tr.classes
-    
-
-def cifar100(batch_size, root):
-
-    transform = transforms.Compose(
-        [transforms.Resize((32, 32)),  # resize to 32x32 pixels
-         transforms.ToTensor()         # convert to tensor. This will also normalize pixels to 0-1
-        ])
-
-    # load train and test sets using torchvision
-    tr   = torchvision.datasets.CIFAR100(root=root, train=True,   download=True, transform=transform)
-    test = torchvision.datasets.CIFAR100(root=root, train=False,  download=True, transform=transform)
-   
-    # Data loaders
-    train_loader = torch.utils.data.DataLoader(tr, batch_size=batch_size,
-                                               shuffle=True, pin_memory=True, num_workers=2)
-    
-    test_loader  = torch.utils.data.DataLoader(test, batch_size=batch_size,
-                                               shuffle=True, pin_memory=True, num_workers=2)
-    
-    return train_loader, test_loader, tr.classes
+    return train_loader, test_loader, tr.classes    
 
 
 if __name__ == "__main__":
@@ -64,8 +45,7 @@ if __name__ == "__main__":
     batch_size = 32
     tr_loader, test_loader, classes = cifar(batch_size=batch_size, cifar_version="CIFAR-10")
 
-    images, labels = next(iter(tr_loader))
-    print(images.shape)
+    images, labels = next(iter(tr_loader)) 
     assert images.shape == (batch_size, 3, 32, 32), "Wrong training set size"
     assert labels.shape == (batch_size,), "Wrong training set size"
 
